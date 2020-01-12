8840264123bae52d7c5b0a249f6b11f5ffc4a5be
Scott W. Vincent <scott@swvincent.com>
2020-01-12 15:58:47 -0500

    Start on context

---
+++ src/fygdb/fygcontext.cs
@@ -0,0 +1,33 @@
+﻿using fygmodels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fygdb
+{
+    class FygContext : DbContext
+    {
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+
+        public FygContext()
+        {
+            //TODO: Should use this or migrations?
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlite(@"Data Source=test.fygamyg;");
+
+            // base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JournalEntry>().ToTable("JournalEntries");
+        }
+    }
+}
+}
