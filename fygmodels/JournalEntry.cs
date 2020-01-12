02d3626edc8d90c22a660b695e6e55f0b9450a1c
Scott W. Vincent <scott@swvincent.com>
2020-01-12 14:40:19 -0500

    Initial commit

---
+++ fygmodels/JournalEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace fygmodels
+{
+    public class JournalEntry
+    {
+        public int ID { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public int AlphaVerse { get; set; }
+        public int OmegaVerse { get; set; }
+        public string Title { get; set; }
+        public string NoteText { get; set; }
+    }
+}
